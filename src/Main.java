d184144d7fd1593b616b38e6499a2b10dcaf011f
aau8996 <auad@mail.uc.edu>
2023-02-24 16:27:26 -0500

    Final Code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        for(int cnt = 0; cnt <= 30; cnt = cnt + 1)
+        {
+            System.out.println(cnt);
+        }
+        for(int cnt = 30; cnt >= 0; cnt = cnt - 1)
+        {
+            System.out.println(cnt);
+        }
+        for(int cnt = 0; cnt <= 18; cnt = cnt + 3)
+        {
+            System.out.println(cnt);
+        }
+        for(int cnt = 10; cnt >= 0; cnt = cnt - 2)
+        {
+            System.out.println(cnt);
+        }
     }
 }
